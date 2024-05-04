a3d4f634b6c8e7885f30ec0bc84fbde714de47cf
Ahmed Hassan <ahmedabdelrhman433@gmail.com>
2024-05-04 15:10:00 +0300

    0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,6 +3,7 @@
 #
 # Setting server ups
 #
+
 new_conf="http {\n\tserver {\n\t\tlisten 80;\n\t\tserver_name 54.80.218.139;\n\t\tlocation /hbnb_static {\n\t\t\talias /data/web_static/current/;\n\t\t\t}\n\t}"
 
 apt update
