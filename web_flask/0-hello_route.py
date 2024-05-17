1b0e29b7834488b249f4ad044a8701c34cd6287e
Ahmed Hassan <ahmedabdelrhman433@gmail.com>
2024-05-17 14:16:57 +0300

    1

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False  # Disable strict slashes for all routes
+
+@app.route('/')
+def index():
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run()
