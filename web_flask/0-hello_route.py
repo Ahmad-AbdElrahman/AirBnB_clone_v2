9d35a64f9605540a1076979d6d75fb9830fd5b39
Ahmed Hassan <ahmedabdelrhman433@gmail.com>
2024-05-17 14:56:16 +0300

    1

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,13 +4,17 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False  # Disable strict slashes for all routes
+
+
+app.url_map.strict_slashes = False
+
 
 @app.route('/')
 def index():
     """Display 'Hello HBNB!'
     """
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
