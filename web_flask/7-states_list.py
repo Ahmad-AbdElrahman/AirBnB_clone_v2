cf73df8ab1ff2472797a17d05a2f870948d981fc
Ahmed Hassan <ahmedabdelrhman433@gmail.com>
2024-05-19 23:41:00 +0300

    flask

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,61 +1,73 @@
 #!/usr/bin/python3
-"""Start a Flask web application
+"""Starts a Flask web application
 """
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-app.url_map.strict_slashes = False
-
-
-@app.route('/')
-def index():
-    """Display 'Hello HBNB!'
-    """
-    return 'Hello HBNB!'
-
-
-@app.route('/hbnb')
-def hbnb():
-    """Display 'HBNB'
-    """
-    return 'HBNB'
-
-
-@app.route('/c/<text>')
-def cisfun(text):
-    """Display “C ” followed by the value of the text variable
-    """
-    return 'C ' + text.replace('_', ' ')
-
-
-@app.route('/python/')
-@app.route('/python/<text>')
-def pythonisfun(text='is cool'):
-    """Display “Python ” followed by the value of the text variable
-    """
-    return 'Python ' + text.replace('_', ' ')
-
-
-@app.route('/number_template/<int:n>')
-def numtemp(n):
-    """Display “n is a number” only if n is an integer
-    """
-    return render_template('5-number.html', number=str(n))
-
-
-@app.route('/number_odd_or_even/<int:n>')
-def numtempodd(n):
-    """Display “n is odd or even” only if n is an integer
-    """
-    even_odd = ""
-    if (n % 2 == 0):
-        even_odd = "even"
-    else:
-        even_odd = "odd"
-    return render_template('6-number_odd_or_even.html',
-                           number=str(n), odd=even_odd)
-
+from flask import Flask
+from flask import render_template
+from models import storage
+from models.state import State
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app = Flask(__name__)
+
+    @app.route('/', strict_slashes=False)
+    def index():
+        """Display 'Hello HBNB!'
+        """
+        return 'Hello HBNB!'
+
+    @app.route('/hbnb', strict_slashes=False)
+    def hbnb():
+        """Display 'HBNB'
+        """
+        return 'HBNB'
+
+    @app.route('/c/<text>', strict_slashes=False)
+    def c(text):
+        """Display “C ” followed by the value of
+        the text variable (replace underscore _
+        symbols with a space)
+        """
+        return 'C ' + text.replace('_', ' ')
+
+    @app.route('/python/')
+    @app.route('/python/<text>', strict_slashes=False)
+    def python(text="is cool"):
+        """Display “Python ”, followed by the value of
+        the text variable (replace underscore _
+        symbols with a space )
+        """
+        return 'Python ' + text.replace('_', ' ')
+
+    @app.route('/number/<int:n>', strict_slashes=False)
+    def number(n):
+        """Display “n is a number” only if n is an integer
+        """
+        return str(n) + ' is a number'
+
+    @app.route('/number_template/<int:n>', strict_slashes=False)
+    def number_template(n):
+        """Display a HTML page only if n is an integer
+        """
+        return render_template('5-number.html', n=n)
+
+    @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+    def number_odd_or_even(n):
+        """Display a HTML page only if n is an integer
+        """
+        parity = 'even' if n % 2 == 0 else 'odd'
+        return render_template('6-number_odd_or_even.html', n=n, parity=parity)
+
+    @app.route('/states_list', strict_slashes=False)
+    def states_list():
+        """Display a HTML page of the States
+        """
+        states = storage.all(State).values()
+        return render_template('7-states_list.html', states=states)
+
+    @app.teardown_appcontext
+    def teardown_db(error):
+        """Closes the database again at the end of the request.
+        """
+        storage.close()
+
+    app.run('0.0.0.0')
